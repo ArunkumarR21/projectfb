963bf90bdc9289936cac1dd900e1364d90c67c61
ArunkumarR21 <Arunbsc21@gmail.com>
2021-04-28 10:37:02 +0530

    add forget password

--- src/test/java/fb/Facebook.java
+++ src/test/java/fb/Facebook.java
@@ -11,8 +11,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Facebook {
-	public static void main(String[] args) throws IOException {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hp\\eclipse-workspace\\fb\\driver\\chromedriver.exe");
+	public static void main(String[] args) throws IOException, InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hp\\eclipse-workspace\\fb1\\projectfb\\driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 		driver.findElement(By.id("email")).sendKeys("Arunb22@gmai.com");
@@ -23,6 +23,8 @@ public class Facebook {
 		System.out.println(as);
 		File des = new File ("C:\\Users\\Hp\\eclipse-workspace\\fb\\Screenshot\\fb.png");
 		FileUtils.copyFile(as, des);
+		Thread.sleep(7000);
+		driver.findElement(By.className("_97w4")).click();
 		
 		
 	} 
